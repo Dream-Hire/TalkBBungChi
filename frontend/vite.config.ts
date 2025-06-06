efed078aed1d43dbadf40e4efc4851981f50f6db
yeon <rk_yeon@naver.com>
2025-06-06 23:49:46 +0900

    init: 프론트엔드 초기 세팅 - eslint : airbnb 적용

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,17 @@
-import * as path from "path";
+import * as path from 'path';
 
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import svgrPlugin from "vite-plugin-svgr";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-	plugins: [react(), svgrPlugin()],
-	server: {
-		port: 3000,
-	},
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [react(), svgr()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
